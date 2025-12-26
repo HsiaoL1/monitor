dbeddfabe5ad38a3f742550fcbbebdd37bc82bf9
HsiaoL1 <shawdml5@gmail.com>
2025-12-26 17:35:24 +0800

    改成测试服

--- go_server/config/config.go
+++ go_server/config/config.go
@@ -39,12 +39,11 @@ func LoadConfig(loginPath string) error {
 	Conf.Services = []models.Service{
 		{Name: "ims_agent_api", Path: "/opt/ims_agent_api", DeployScript: "./deploy.sh", MemoryThresholdMB: 15360, AutoRestart: true},
 		{Name: "ims_server_api", Path: "/opt/ims_server_api", DeployScript: "./deploy.sh", MemoryThresholdMB: 15360, AutoRestart: true},
-		{Name: "ims_server_active", Path: "/opt/ims_server_active", DeployScript: "./deploy.sh", MemoryThresholdMB: 15360, AutoRestart: true},
 		{Name: "ims_server_send", Path: "/opt/ims_server_send/cmd/ims_server_send", DeployScript: "./deploy.sh", MemoryThresholdMB: 15360, AutoRestart: true},
 		{Name: "ims_server_task", Path: "/opt/ims_server_task/cmd/ims_server_task", DeployScript: "./deploy.sh", MemoryThresholdMB: 15360, AutoRestart: true},
-		{Name: "ims_server_web", Path: "/opt/ims_server_web/cmd/server", DeployScript: "./deploy.sh", PprofURL: "http://119.8.54.133:9090/debug/pprof/", MemoryThresholdMB: 15360, AutoRestart: true},
-		{Name: "ims_server_ws", Path: "/opt/ims_server_ws/cmd/server", DeployScript: "./deploy.sh", PprofURL: "http://119.8.54.133:9000/debug/pprof/", MemoryThresholdMB: 15360, AutoRestart: true},
-		{Name: "ims_server_mq", Path: "/opt/ims_server_mq/cmd/mq", DeployScript: "./deploy.sh", PprofURL: "http://119.8.54.133:9002/debug/pprof/", MemoryThresholdMB: 15360, AutoRestart: true},
+		{Name: "ims_server_web", Path: "/opt/new_ims/ims_server_web/cmd/server", DeployScript: "./deploy.sh", PprofURL: "http://47.242.170.252:9090/debug/pprof/", MemoryThresholdMB: 15360, AutoRestart: true},
+		{Name: "ims_server_ws", Path: "/opt/new_ims/ims_server_ws/cmd/server", DeployScript: "./deploy.sh", PprofURL: "http://47.242.170.252:9000/debug/pprof/", MemoryThresholdMB: 15360, AutoRestart: true},
+		{Name: "ims_server_mq", Path: "/opt/new_ims/ims_server_mq/cmd/mq", DeployScript: "./deploy.sh", PprofURL: "http://47.242.170.252:9002/debug/pprof/", MemoryThresholdMB: 15360, AutoRestart: true},
 	}
 
 	// Initialize Redis config
@@ -52,7 +51,7 @@ func LoadConfig(loginPath string) error {
 		Host:     "127.0.0.1",
 		Port:     6379,
 		Password: "smmtk@9988",
-		DB:       0,
+		DB:       5,
 	}
 
 	return nil
